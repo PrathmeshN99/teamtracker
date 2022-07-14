cf04112d2f14c2b5b47e0ee6d9c82edfcd235775
PrathmeshN99 <90515944+PrathmeshN99@users.noreply.github.com>
2022-07-14 15:16:26 +0530

    started project

---
+++ trackApp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from trackApp import views
+
+urlpatterns = [
+    path('',views.login,name='login'),
+    path('register',views.register,name='register'),
+    path('home',views.home,name='home'),
+]
---
+++ trackApp/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
+
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']   
+        last_name = request.POST['last_name']
+        user_name = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        
+        if User.objects.all().filter(username=user_name).exists():
+            print("Username already exists")
+            return redirect('/register')
+
+        else:    
+            user = User.objects.create_user(username=user_name,first_name=first_name,last_name=last_name,email=email,password=password)
+            user.save()
+            print("Created user " + user_name)
+            return redirect('/')
+    else:
+        return render(request,'register.html')
+ 
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username,password=password)
+        print("user : ")
+        print(user)
+        
+        if user is not None:
+            auth.login(request,user)
+            print("logged in")
+            # return render(request,'home.html')
+            request.session['username']=username
+            return redirect('/home')
+        else:
+
+            print("Please enter correct credentials")
+            return redirect('/')
+    else:
+        return render(request,'login.html')
+
+def home(request):
+    return render(request,'home.html')
