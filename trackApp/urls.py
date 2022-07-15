8656263e7af7eb5c44387b3786bd71b0b1a561aa
Harshwardhan More <87003053+HarshwardhanMore@users.noreply.github.com>
2022-07-14 22:59:11 -0700

    .

--- trackApp/urls.py
+++ trackApp/urls.py
@@ -3,6 +3,7 @@ from trackApp import views
 
 urlpatterns = [
     path('',views.login,name='login'),
+    path('login',views.login,name='login'),
     path('register',views.register,name='register'),
     path('home',views.home,name='home'),
 ]
