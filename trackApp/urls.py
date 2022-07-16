850fe6d90744cb49595b76371254f502829d94be
Harshwardhan More <87003053+HarshwardhanMore@users.noreply.github.com>
2022-07-16 03:06:27 -0700

    .

--- trackApp/urls.py
+++ trackApp/urls.py
@@ -9,8 +9,15 @@ urlpatterns = [
     path('profile',views.profile,name='profile'),
     path('aboutapp',views.aboutapp,name='aboutapp'),
     path('quickguide',views.quickguide,name='quickguide'),
+<<<<<<< HEAD
     path('logout',views.logout_view,name="logout_view"),
     path('createproject',views.createproject,name="createproject"),
     path('joinproject',views.joinproject,name="joinproject"),
     path('currentprojects',views.currentprojects,name="currentprojects"),
+=======
+    path('logout',views.logout_view,name='logout_view'),
+    path('createProject',views.createProject,name='createProject'),
+    path('joinProject',views.joinProject,name='joinProject'),
+    path('myProjects',views.myProjects,name='myProjects'),
+>>>>>>> 1e1f36778459fd90ff9cf122691b8aa021c2e867
 ]
